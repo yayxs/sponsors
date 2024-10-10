62fb76a2afec7a1f39db6dc16fcf4bcd0c43a0ba
yayxs <leaderywl@163.com>
2024-10-10 17:13:49 +0800

    chore: add vite-react-ssg

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { ViteReactSSG } from 'vite-react-ssg'
+import { routes } from './routes'
+
+export const createRoot = ViteReactSSG({ routes })
---
+++ src/pages/index.tsx
@@ -0,0 +1,3 @@
+export default function Layout() {
+  return <div>Hello 22222</div>
+}
